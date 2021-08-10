b4c5a6042d99c80f2376d979cb4e9536cb8801fe
MacbookAir <fmdentiste@pm.me>
2021-08-10 15:04:28 +0200

    function deploy JSON

--- js/recipes.js
+++ js/recipes.js
@@ -3,7 +3,30 @@ const recipesSection = document.querySelector('.recipes')
 const ingredientsMenu = document.querySelector('.ingredients__menu')
 const applianceMenu = document.querySelector('.appliance__menu')
 const ustensilsMenu = document.querySelector('.ustensils__menu')
+const btnSearchNodes = document.querySelectorAll('.btn__group__search')
+const btnSearch = Array.from(btnSearchNodes)
+const inputsSearchNodes = document.querySelectorAll('.input__search')
+const inputsSearch = Array.from(inputsSearchNodes)
+let allRecipes = {}
 
+// EventListener for displaying search inputs
+btnSearch.forEach(element => element.addEventListener('focusin', openSearchInput))
+inputsSearch.forEach(element => element.addEventListener('focusout', closeSearchInput))
+
+function openSearchInput(){
+    this.firstElementChild.nextElementSibling.classList.replace('d-none', 'd-block')
+    this.firstElementChild.classList.replace('d-block', 'd-none')
+    this.lastElementChild.classList.replace('d-none', 'd-block')
+    this.firstElementChild.nextElementSibling.focus()
+}
+
+function closeSearchInput(){
+    this.parentElement.firstElementChild.nextElementSibling.classList.replace('d-block', 'd-none')
+    this.parentElement.firstElementChild.classList.replace('d-none', 'd-block')
+    this.parentElement.lastElementChild.classList.replace('d-block', 'd-none')
+}
+
+// Ul creation for Ingredients/Appliance/Ustensils
 let newIngredientUl = document.createElement('ul')
 ingredientsMenu.appendChild(newIngredientUl)
 let newIngredientsArray = []
@@ -16,11 +39,15 @@ let newUstensilsUl = document.createElement('ul')
 ustensilsMenu.appendChild(newUstensilsUl)
 let newUstensilsArray = []
 
+// JSON extraction
 fetch(myRequest)
 .then(response => response.json())
 .then(function extractRecipes(data){
-    for (let i = 0; i < data.recipes.length; i++){
+    allRecipes = data
+    deployJSON(data)
+    /*for (let i = 0; i < data.recipes.length; i++){
         // Adding all recipies to recipes section
+            // first line of the recipe
         let newRecipe = document.createElement('div')
         recipesSection.appendChild(newRecipe)
         newRecipe.classList.add('recipe')
@@ -36,6 +63,8 @@ fetch(myRequest)
         let newH3 = document.createElement('h3')
         newRecipeDetails.appendChild(newH3)
         newH3.innerHTML = `<i class="far fa-clock"></i> ${data.recipes[i].time} min`
+
+            // list of ingredients + recipe
         let newUl = document.createElement('ul')
         newRecipeDetails.appendChild(newUl)
         let allIngredients = data.recipes[i].ingredients
@@ -53,12 +82,17 @@ fetch(myRequest)
         let newP = document.createElement('p')
         newRecipeDetails.appendChild(newP)
         newP.textContent = data.recipes[i].description
+            // adding data attributes for appliance & ustensils
         let allUstensils = data.recipes[i].ustensils
-        // adding all ingredients/appliance/ustensils to the ingredients input
+        newRecipe.setAttribute('data-appliance', data.recipes[i].appliance)
+        newRecipe.setAttribute('data-ustensils', allUstensils)
+
+            // adding all ingredients/appliance/ustensils to the ingredients inputs
         for(let i = 0; i < allIngredients.length; i++){
             if(! newIngredientsArray.includes(allIngredients[i].ingredient)){
                 newIngredientsArray.push(allIngredients[i].ingredient)
                 let newLi = document.createElement('li')
+                newLi.classList.add('ingredient')
                 newIngredientUl.appendChild(newLi)
                 newLi.textContent = allIngredients[i].ingredient
             } 
@@ -66,6 +100,7 @@ fetch(myRequest)
         if(! newApplianceArray.includes(data.recipes[i].appliance)){
             newApplianceArray.push(data.recipes[i].appliance)
             let newLi = document.createElement('li')
+            newLi.classList.add('appliance')
             newApplianceUl.appendChild(newLi)
             newLi.textContent = data.recipes[i].appliance
         }
@@ -73,10 +108,83 @@ fetch(myRequest)
             if(! newUstensilsArray.includes(allUstensils[i])){
                 newUstensilsArray.push(allUstensils[i])
                 let newLi = document.createElement('li')
+                newLi.classList.add('ustensil')
+                newUstensilsUl.appendChild(newLi)
+                newLi.textContent = allUstensils[i]
+            } 
+        }
+    }*/ 
+    }
+    );
+
+function deployJSON(data){
+    for (let i = 0; i < data.recipes.length; i++){
+        // Adding all recipies to recipes section
+            // first line of the recipe
+        let newRecipe = document.createElement('div')
+        recipesSection.appendChild(newRecipe)
+        newRecipe.classList.add('recipe')
+        let newRecipeImg = document.createElement('div')
+        newRecipe.appendChild(newRecipeImg)
+        newRecipeImg.classList.add('recipe__img')
+        let newRecipeDetails = document.createElement('div')
+        newRecipe.appendChild(newRecipeDetails)
+        newRecipeDetails.classList.add('recipe__details')
+        let newH2 = document.createElement('h2')
+        newRecipeDetails.appendChild(newH2)
+        newH2.textContent = data.recipes[i].name
+        let newH3 = document.createElement('h3')
+        newRecipeDetails.appendChild(newH3)
+        newH3.innerHTML = `<i class="far fa-clock"></i> ${data.recipes[i].time} min`
+
+            // list of ingredients + recipe
+        let newUl = document.createElement('ul')
+        newRecipeDetails.appendChild(newUl)
+        let allIngredients = data.recipes[i].ingredients
+        allIngredients.forEach(element => {
+            let newLi = document.createElement('li')
+            newUl.appendChild(newLi)
+            if(element.unit != undefined && element.quantity != undefined){
+                newLi.innerHTML = `<strong>${element.ingredient} : </strong> ${element.quantity} ${element.unit}`
+            } else if (element.quantity != undefined){
+                newLi.innerHTML = `<strong>${element.ingredient} : </strong> ${element.quantity}`
+            } else {
+                newLi.innerHTML = `<strong>${element.ingredient}</strong>`
+            }
+        });
+        let newP = document.createElement('p')
+        newRecipeDetails.appendChild(newP)
+        newP.textContent = data.recipes[i].description
+            // adding data attributes for appliance & ustensils
+        let allUstensils = data.recipes[i].ustensils
+        newRecipe.setAttribute('data-appliance', data.recipes[i].appliance)
+        newRecipe.setAttribute('data-ustensils', allUstensils)
+
+            // adding all ingredients/appliance/ustensils to the ingredients inputs
+        for(let i = 0; i < allIngredients.length; i++){
+            if(! newIngredientsArray.includes(allIngredients[i].ingredient)){
+                newIngredientsArray.push(allIngredients[i].ingredient)
+                let newLi = document.createElement('li')
+                newLi.classList.add('ingredient')
+                newIngredientUl.appendChild(newLi)
+                newLi.textContent = allIngredients[i].ingredient
+            } 
+        }
+        if(! newApplianceArray.includes(data.recipes[i].appliance)){
+            newApplianceArray.push(data.recipes[i].appliance)
+            let newLi = document.createElement('li')
+            newLi.classList.add('appliance')
+            newApplianceUl.appendChild(newLi)
+            newLi.textContent = data.recipes[i].appliance
+        }
+        for(let i = 0; i < allUstensils.length; i++){
+            if(! newUstensilsArray.includes(allUstensils[i])){
+                newUstensilsArray.push(allUstensils[i])
+                let newLi = document.createElement('li')
+                newLi.classList.add('ustensil')
                 newUstensilsUl.appendChild(newLi)
                 newLi.textContent = allUstensils[i]
             } 
         }
     } 
-    }
-    );
+}
